3ea6c7d25333f330145b6e2e18452d3c8a6e7262
wu <844155339@qq.com>
2021-02-02 17:51:11 +0800

    feat: 验证码随机数，密码加密函数

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import { parse } from 'querystring';
+import * as CryptoJS from 'crypto-js'
 
 /* eslint no-useless-escape:0 import/prefer-default-export:0 */
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
@@ -22,3 +23,44 @@ export const isAntDesignProOrDev = (): boolean => {
 };
 
 export const getPageQuery = () => parse(window.location.href.split('?')[1]);
+
+export const randomLenNum = (len: number, date?: boolean) => {
+  let random = ''
+  random = Math.ceil(Math.random() * 100000000000000).toString().substr(0, len || 4)
+  if (date) random = random + Date.now()
+  return Number(random)
+}
+
+/**
+ *加密处理
+ */
+export const encryption = (params: any) => {
+  let {
+    data,
+    type,
+    param,
+    key
+  } = params
+  const result = JSON.parse(JSON.stringify(data))
+  if (type === 'Base64') {
+    param.forEach((ele: any) => {
+      result[ele] = btoa(result[ele])
+    })
+  } else {
+    param.forEach((ele: any) => {
+      var data = result[ele]
+      key = CryptoJS.enc.Latin1.parse(key)
+      var iv = key
+      // 加密
+      var encrypted = CryptoJS.AES.encrypt(
+        data,
+        key, {
+          iv: iv,
+          mode: CryptoJS.mode.CBC,
+          padding: CryptoJS.pad.ZeroPadding
+        })
+      result[ele] = encrypted.toString()
+    })
+  }
+  return result
+}
