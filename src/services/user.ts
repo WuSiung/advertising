365ed58f8d5c1a2307ee6fbdd2934a86f2bdb8d7
wu <844155339@qq.com>
2021-02-03 23:02:51 +0800

    feat: 用户信息及Facebook API定义

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,13 +1,25 @@
 import request from '@/utils/request';
+import type {FacebookAccount} from '@/models/user'
 
 export async function query(): Promise<any> {
   return request('/api/users');
 }
 
 export async function queryCurrent(): Promise<any> {
-  return request('/api/currentUser');
+  return request('/admin/user/info');
+}
+
+export async function queryFbAccounts(): Promise<any> {
+  return request('/admin/systoken/getTokens');
 }
 
 export async function queryNotices(): Promise<any> {
   return request('/api/notices');
+}
+
+export async function changeFbAccount(params:FacebookAccount) {
+  return request('/admin/systoken/bindPlatform', {
+    data: params,
+    method: 'post'
+  })
 }
