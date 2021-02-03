84dc831a8d1baaaf443ecfa3f4a0118d45d7658c
wu <844155339@qq.com>
2021-02-03 22:59:59 +0800

    feat: 新增app信息，用户信息，Facebook账号列表数据类型，保存

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,23 +1,43 @@
 import type { Effect, Reducer } from 'umi';
 
-import { queryCurrent, query as queryUsers } from '@/services/user';
+import { queryCurrent, query as queryUsers, queryFbAccounts } from '@/services/user';
 
 export type CurrentUser = {
   avatar?: string;
-  name?: string;
-  title?: string;
-  group?: string;
-  signature?: string;
-  tags?: {
-    key: string;
-    label: string;
-  }[];
-  userid?: string;
+  username?: string;
+  createTime?: string;
+  delFlag?: string;
+  deptId?: string;
+  fbid?: string,
+  lockFlag?: string,
+  phone?: string
+  userId?: number;
   unreadCount?: number;
 };
 
+export type AppInfo = {
+  appId?: string,
+  appName?: string,
+  fbId?: string,
+  logo?: string,
+  type?: string,
+  url?: string
+}
+
+export type FacebookAccount = {
+  accessToken?: string,
+  accountId?: string,
+  accountName?: string,
+  checked?: string,
+  fbid?: string,
+  deptId?: string,
+  tokenId?: number
+}
+
 export type UserModelState = {
-  currentUser?: CurrentUser;
+  currentUser?: CurrentUser,
+  appInfo?: AppInfo,
+  facebookAccounts?: FacebookAccount[]
 };
 
 export type UserModelType = {
@@ -26,10 +46,13 @@ export type UserModelType = {
   effects: {
     fetch: Effect;
     fetchCurrent: Effect;
+    fetchFbAccounts: Effect;
   };
   reducers: {
-    saveCurrentUser: Reducer<UserModelState>;
+    saveCurrentUser: Reducer<UserModelState['currentUser']>;
     changeNotifyCount: Reducer<UserModelState>;
+    saveAppInfo: Reducer<UserModelState['appInfo']>;
+    saveFbAccounts: Reducer<UserModelState>
   };
 };
 
@@ -38,6 +61,8 @@ const UserModel: UserModelType = {
 
   state: {
     currentUser: {},
+    appInfo: {},
+    facebookAccounts: []
   },
 
   effects: {
@@ -50,11 +75,26 @@ const UserModel: UserModelType = {
     },
     *fetchCurrent(_, { call, put }) {
       const response = yield call(queryCurrent);
-      yield put({
-        type: 'saveCurrentUser',
-        payload: response,
-      });
+      try {
+        yield put({
+          type: 'saveCurrentUser',
+          payload: response.data.sysUser,
+        });
+        yield put({
+          type: 'saveAppInfo',
+          payload: response.data.advApp,
+        });
+      } catch (err) {
+        console.log(err)
+      }
     },
+    *fetchFbAccounts(_, { call, put }) {
+      const response = yield call(queryFbAccounts);
+      yield put({
+        type: 'saveFbAccounts',
+        payload: response.data
+      })
+    }
   },
 
   reducers: {
@@ -64,9 +104,23 @@ const UserModel: UserModelType = {
         currentUser: action.payload || {},
       };
     },
+    saveAppInfo(state, {payload}) {
+      return {
+        ...state,
+        appInfo: payload || {}
+      }
+    },
+    saveFbAccounts(state, { payload }) {
+      return {
+        ...state,
+        facebookAccounts: payload
+      }
+    },
     changeNotifyCount(
       state = {
         currentUser: {},
+        appInfo: {},
+        facebookAccounts: [],
       },
       action,
     ) {
