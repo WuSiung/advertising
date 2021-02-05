64a9a76740360b676d43f5e75f44cee921dbeb14
wu <844155339@qq.com>
2021-02-05 19:29:42 +0800

    fix: 左侧菜单栏title修改

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -13,7 +13,7 @@ const proSettings: DefaultSettings = {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: '智能广告平台',
   pwa: false,
   iconfontUrl: '',
 };
