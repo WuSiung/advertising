66c318a41c0f3de768f6a4ab17deed667c6fdb00
wu <844155339@qq.com>
2021-02-06 17:16:54 +0800

    feat: 工作台上传文本功能完善

--- src/pages/adv-launcher/workbench/components/ActionBtns/index.tsx
+++ src/pages/adv-launcher/workbench/components/ActionBtns/index.tsx
@@ -1,16 +1,20 @@
 import { UploadRequestOption as RcCustomRequestOptions } from 'rc-upload/lib/interface';
-import { Col, Row, Button, Select, Upload, Spin } from 'antd'
+import { Col, Row, Button, Select, Upload, Spin, message } from 'antd'
 import React, { FC, useCallback, useState } from 'react'
 import { connect, Dispatch } from 'umi';
 import Loading from '@/components/Loading'
+import { isImage, isVideo } from '@/utils/fileType'
+import UploadTextFile from '../../../components/UploadTextFile'
 import { postOneRecordToWorkbench } from '../../service'
-import { PostMediaDataType } from '../../data';
+import { PostMediaDataType, WorkbenchDataType } from '../../data';
 
 import styles from './index.less'
 import { RcFile } from 'antd/lib/upload';
 
 interface ActionBtnsProps {
+    workbench: WorkbenchDataType,
     upFileloading?: boolean,
+    upTextLoading?: boolean,
     dispatch: Dispatch
 }
 const uploadSucessValue: void[] = []
@@ -18,16 +22,20 @@ let uploadFileLength = 0;
 let uploadedLenth: number = 0
 const ActionBtns: FC<ActionBtnsProps> = (props) => {
 
-    const { dispatch, upFileloading } = props
+    const { dispatch, upFileloading, upTextLoading, workbench } = props
+
+    const [modalVisible, setModalVisible] = useState<boolean>(false)
 
     const fileChange = async (e: RcCustomRequestOptions, dispatch: Dispatch) => {
         const formData: FormData = new FormData()
         formData.append('media', e.file)
+        const type: 0 | 1 | undefined = fileType(e.file.type)
         if (dispatch) {
             const res: PostMediaDataType = await dispatch({
                 type: 'workbench/uploadFile',
                 payload: formData
             })
+            res.type = type
             uploadedLenth++
             uploadSucessValue.push(addResultToWorkbench(res))
             if (uploadFileLength == uploadedLenth) {
@@ -45,12 +53,15 @@ const ActionBtns: FC<ActionBtnsProps> = (props) => {
         <div className={styles.actions}>
             <Row>
                 <Col span={12}>
+                    <UploadTextFile visible={modalVisible} submitText={value => submitText(value, dispatch, setModalVisible, workbench)}
+                        loading={upTextLoading} setVisible={setModalVisible}>
+                    </UploadTextFile>
                     <div className={styles.uploadBtns}>
                         <Upload multiple customRequest={e => fileUpload(e, dispatch)} showUploadList={false} beforeUpload={setUploadLength}>
                             <Button type="primary" disabled={upFileloading}>上传资源</Button>
                         </Upload>
                     </div>
-                    <Button type="primary">新增标题</Button>
+                    <Button type="primary" onClick={() => setModalVisible(true)}>新增标题</Button>
                     <Button type="primary">保存模板</Button>
                     <Select style={{ minWidth: 200 }}>
                         <Select.Option value='2'>11</Select.Option>
@@ -68,6 +79,19 @@ const ActionBtns: FC<ActionBtnsProps> = (props) => {
         </div>
     )
 }
+const submitText = async (value: string[], dispatch: Dispatch, handleModalVisible: (visible: boolean) => void, workbench: WorkbenchDataType) => {
+    const postArr = value.map(text => {
+        const textStr = text.split('&&')
+        return { title: textStr[1] || '', content: textStr[0] || '' }
+    })
+    await dispatch({
+        type: 'workbench/uploadText',
+        payload: postArr.concat(workbench.uploadTextList)
+    })
+    message.success('上传成功')
+    dispatch({ type: 'workbench/fetchAllList' })
+    handleModalVisible(false)
+}
 
 const setUploadLength = (file: RcFile, fileList: RcFile[]): boolean => {
     uploadFileLength = fileList.length
@@ -78,9 +102,20 @@ const addResultToWorkbench = (result: PostMediaDataType) => {
     postOneRecordToWorkbench(result)
 }
 
+const fileType = (name: string) => {
+    if (isImage(name)) {
+        return 0
+    } else if (isVideo(name)) {
+        return 1
+    } else {
+        return
+    }
+}
 
 
-export default connect(({ workbench, loading }: { workbench: any, loading: { effects: { [key: string]: boolean } } }) => ({
+
+export default connect(({ workbench, loading }: { workbench: WorkbenchDataType, loading: { effects: { [key: string]: boolean } } }) => ({
     workbench,
-    upFileloading: loading.effects['workbench/uploadFile']
+    upFileloading: loading.effects['workbench/uploadFile'],
+    upTextLoading: loading.effects['workbench/uploadText'],
 }))(ActionBtns)
