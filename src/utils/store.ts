1a8e092642edef0cfd4712e42a1343b32ed7e857
wu <844155339@qq.com>
2021-02-26 17:16:57 +0800

    feat: 缓存refresh_token

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,12 +1,20 @@
 
 class Store {
     static tokenKey: string = 'token';
+    static refreshTokenKey: string = 'refresh_token'
 
     static GetToken(): string | null {
         return localStorage.getItem(this.tokenKey);
     }
     static SetToken(tokenValue: string) {
         localStorage.setItem(this.tokenKey, tokenValue)
+    }
+
+    static GetRefreshToken(): string | null{
+        return localStorage.getItem(this.refreshTokenKey);
+    }
+    static SetRefreshToken(refreshTokenValue: string) {
+        localStorage.setItem(this.refreshTokenKey, refreshTokenValue)
     }
 
     public static clearStorage() {
