275943ecea03cfed9c5a9f0a3632e7b774dd1fa9
ys_adv <ys_adv123456>
2021-02-27 15:03:05 +0800

    feat:已有广告功能

--- src/pages/adv-launcher/advertising/index.tsx
+++ src/pages/adv-launcher/advertising/index.tsx
@@ -1,6 +1,7 @@
-import { Card } from 'antd'
-import React, { FC } from 'react'
+import {Card} from 'antd'
+import React, {FC} from 'react'
 import DateRange from '../components/DateRange';
+import {AdvPreview} from "../components/AdvPreview";
 
 import styles from './index.less'
 
@@ -8,8 +9,46 @@ const Advertising: FC<any> = (props) => {
     return (
         <Card>
             <div className={styles.filter}>
-                <span className={styles.date}>发布范围筛选： <DateRange /></span>
-                <span className={styles.date}>统计数据过滤： <DateRange /></span>
+                <span className={styles.date}>发布范围筛选： <DateRange/></span>
+                <span className={styles.date}>统计数据过滤： <DateRange/></span>
+            </div>
+            <div className={styles.advWarp}>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                            title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
+                <AdvPreview  classNames={styles.advPreview} url="baidu.com" content="这是内容这是内容这是内容这是内容这是内容这是内容这是内容，这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容"
+                             title="这是标题，这是标题，这是标题，这是标题，" imgId={123} textId={321}/>
             </div>
         </Card>
     )
