d436e71f65368b5d7efe533e53ed0253dacf9b7a
ys_adv <ys_adv123456>
2021-03-01 09:28:00 +0800

    feat:已有广告功能

--- src/pages/adv-launcher/advertising/index.tsx
+++ src/pages/adv-launcher/advertising/index.tsx
@@ -1,108 +1,106 @@
-import { Card, Button } from 'antd'
-import React, { FC, useEffect, useState } from 'react'
+import {Card,Button} from 'antd'
+import React, {FC,useState} from 'react'
 import DateRange from '../components/DateRange';
-import { AdvPreview } from "../components/AdvPreview";
+import {AdvPreview} from "../components/AdvPreview";
 
 import styles from './index.less'
-import { connect, AppInfo } from "umi";
-import { AdvModelStateType } from "./data";
-import { AdvAdvListType } from "@/pages/adv-manager/data";
-import { Dispatch, UserModelState } from "@@/plugin-dva/connect";
-import Loading from "@/components/Loading";
+import {connect,AppInfo} from "umi";
+import {AdvModelStateType} from "./data";
+import {AdvAdvListType} from "@/pages/adv-manager/data";
+import {Dispatch, UserModelState} from "@@/plugin-dva/connect";
+import  Loading  from "@/components/Loading";
 
 interface AdvPropsType {
     advertisingList: AdvAdvListType[],
     loadingAdvList: boolean,
     dispatch: Dispatch,
-    appInfo: AppInfo | undefined,
-    loadingAdvListAddMore: boolean,
-    count: number
+    appInfo:AppInfo|undefined,
+    loadingAdvListAddMore:boolean,
+    count:number
 }
-const pageSize: number = 100;
+const pageSize:number=20;
 const Advertising: FC<AdvPropsType> = (props) => {
-    const { dispatch, advertisingList, appInfo, loadingAdvList, count, loadingAdvListAddMore } = props;
-    const [pageIndex, setPageIndex] = useState<number>(1);
-    const [date, setDate] = useState<[string, string]>();
-    const [dateT, setDateT] = useState<[string, string]>();
-    console.log(2)
-    useEffect(() => {
-        
-    }, [])
+    const {dispatch,advertisingList,appInfo,loadingAdvList,count,loadingAdvListAddMore} =props;
+    const [pageIndex,setPageIndex] = useState<number>(1);
+    const [date,setDate] = useState<[string,string]>();
+    const [dateT,setDateT] = useState<[string,string]>();
     return (
         <Card>
-            {advertisingList.length + '---' + count}
             <div className={styles.filter}>
-                <span className={styles.date}>发布范围筛选： <DateRange onChange={(dateStr) => {
+                <span className={styles.date}>发布范围筛选： <DateRange onChange={(dateStr)=>{
                     setDate(dateStr);
                     dispatch({
                         type: 'advertising/fetchAdvList',
                         payload: {
-                            current: pageIndex,
-                            start: dateStr[0],
-                            end: dateStr[1],
-                            size: pageSize
+                            current:pageIndex,
+                            start:dateStr[0],
+                            end:dateStr[1],
+                            size:pageSize
                         }
                     });
-                }} /></span>
-                <span className={styles.date}>统计数据过滤： <DateRange onChange={(dateStr) => {
+                }}/></span>
+                <span className={styles.date}>统计数据过滤： <DateRange onChange={(dateStr)=>{
                     setDateT(dateStr);
                     dispatch({
                         type: 'advertising/fetchAdvList',
                         payload: {
-                            current: pageIndex,
-                            startT: dateStr[0],
-                            endT: dateStr[1],
-                            size: pageSize
+                            current:pageIndex,
+                            /*startT:dateStr[0],
+                            endT:dateStr[1],*/
+                            size:pageSize
                         }
                     });
-                }} /></span>
+                }}/></span>
             </div>
-            {(loadingAdvList || loadingAdvListAddMore) && <Loading size="large" />}
-            <div className={styles.advWarp}>
-                {
-                    advertisingList.map(advertings => {
-                        const imgtext = advertings.imgTextList[0] ? advertings.imgTextList[0] : null;
-                        return (<div style={{ border: "1px solid #d9d9d9" }} className={styles.advPreviewWrap}>
+            {(loadingAdvList||loadingAdvListAddMore)&&<Loading size="large" />}
+            <div>
+                <div className={styles.advWarp}>
 
-                            <AdvPreview appInfo={appInfo} classNames={styles.advPreview} url={imgtext?.advImg.url} content={imgtext?.advText.content}
-                                title={imgtext?.advText.title} type={imgtext?.advImg.type} imgId={imgtext?.advImg.imgId} textId={imgtext?.advText.textId} />
-                            <div className={styles.mask}>
-                                <div className={styles.top}>
-                                    消费金额 : {advertings.spent} 点击率 ：{advertings.ctr} <br /> 广告支出回报率 : {advertings.approas}
+                    {
+                        advertisingList.map(advertings=>{
+                            const imgtext=advertings.imgTextList[0]?advertings.imgTextList[0]:null;
+                            return (<div style={{border:"1px solid #d9d9d9"}} className={styles.advPreviewWrap}>
+
+                                <AdvPreview  appInfo={appInfo}  classNames={styles.advPreview} url={imgtext?.advImg.url} content={imgtext?.advText.content}
+                                             title={imgtext?.advText.title} type={imgtext?.advImg.type} imgId={imgtext?.advImg.imgId} textId={imgtext?.advText.textId}/>
+                                <div className={styles.mask}>
+                                    <div className={styles.top}>
+                                        消费金额 : {advertings.spent} 点击率 ：{advertings.ctr} <br/> 广告支出回报率 : {advertings.approas}
+                                    </div>
+                                    <div className={styles.bottom}>
+                                        预览广告
+                                    </div>
                                 </div>
-                                <div className={styles.bottom}>
-                                    预览广告
-                                </div>
-                            </div>
-                        </div>)
-                    })
-                }
+                            </div>)
+                        })
+                    }
+                </div>
             </div>
             {
-                advertisingList.length < count ? <Button onClick={() => {
+                advertisingList.length<count?<Button  loading={loadingAdvListAddMore} onClick={()=>{
                     dispatch({
                         type: 'advertising/fetchAdvListAddMore',
                         payload: {
-                            startT: dateT ? dateT[0] : "",
-                            endT: dateT ? dateT[1] : "",
-                            start: date ? date[0] : "",
-                            end: date ? date[1] : "",
-                            current: pageIndex + 1,
-                            size: pageSize
+                            startT:dateT?dateT[0]:"",
+                            endT:dateT?dateT[1]:"",
+                            start:date?date[0]:"",
+                            end:date?date[1]:"",
+                            current:pageIndex+1,
+                            size:pageSize
                         }
                     });
-                    setPageIndex(pageIndex + 1);
-                }}>加载更多</Button> : <></>
+                    setPageIndex(pageIndex+1);
+                }}>加载更多</Button>:<></>
             }
         </Card>
     )
 }
 
-export default connect(({ advertising, loading, user }: { user: UserModelState, advertising: AdvModelStateType, loading: { effects: { [key: string]: boolean } } }) => ({
+export default connect(({advertising, loading, user}: { user:UserModelState, advertising: AdvModelStateType, loading: { effects: { [key: string]: boolean } } }) => ({
     advertising,
-    advertisingList: advertising.advertisingList,
+    advertisingList:advertising.advertisingList,
     appInfo: user.appInfo,
-    count: advertising.count,
+    count:advertising.count,
     loadingAdvList: loading.effects['advertising/fetchAdvList'],
     loadingAdvListAddMore: loading.effects['advertising/fetchAdvListAddMore']
 }))(Advertising)
