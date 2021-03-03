c405677c3daadb91941769f36c031e0333e0080f
t-vic <t-vick@msn.com>
2021-03-03 10:49:48 +0800

    feat:抽取可切换指标配置

---
+++ src/pages/dashboard/targets.ts
@@ -0,0 +1,57 @@
+export const TARGET_LIST = [
+  {
+    name: '每结果成本',
+    value: 'pfee',
+  },
+  {
+    name: '移动应用回报率',
+    value: 'approas',
+  },
+  {
+    name: '消费金额',
+    value: 'spent',
+  },
+  {
+    name: '点击率',
+    value: 'ctr',
+  },
+  {
+    name: '频率',
+    value: 'frequency',
+  },
+  {
+    name: '费用/前次',
+    value: 'cpm',
+  },
+  {
+    name: '出站点击率',
+    value: 'octr',
+  },
+  {
+    name: '出站点击数',
+    value: 'oclicks',
+  },
+  {
+    name: '每次点击费用',
+    value: 'cpc',
+  },
+  {
+    name: '安装数',
+    value: 'installs',
+  },
+  // {
+  //   name: '每次安装费用',
+  //   value: 'installfee'
+  // }, // installfee暂时为null
+];
+
+export const ROI_TARGET_LIST = [
+  {
+    value: 'income',
+    name: '收入',
+  },
+  {
+    value: 'spend',
+    name: '支出',
+  },
+];
