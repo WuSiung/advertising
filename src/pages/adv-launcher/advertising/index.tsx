a86a04a16a183217552bcdbcb1b470d2480588a2
ys_adv <ys_adv123456>
2021-03-03 18:53:00 +0800

    fix:已有广告 筛选条件小屏上排列

--- src/pages/adv-launcher/advertising/index.tsx
+++ src/pages/adv-launcher/advertising/index.tsx
@@ -110,166 +110,172 @@ const Advertising: FC<AdvPropsType> = (props) => {
                 </div>
             </Modal> : <></>}
             <div className={styles.filter}>
-                <div>
-                    评分排序 :&nbsp;
-                    <Select
-                        showSearch
-                        style={{width: 200}}
-                        placeholder="选择排序字段"
-                        optionFilterProp="children"
-                        filterOption={(input: string, option) =>
-                            option?.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                        }
-                    >
-                        <Select.Option value="approas">广告回报率</Select.Option>
-                        <Select.Option value="ctr">点击率</Select.Option>
-                        <Select.Option value="frequency">频率</Select.Option>
-                        <Select.Option value="impression">展示数</Select.Option>
-                        <Select.Option value="cpc">每次点击费用</Select.Option>
-                        <Select.Option value="cpm">费用/千次</Select.Option>
-                        <Select.Option value="installs">安装次数</Select.Option>
-                        <Select.Option value="installfee">每次安装费用</Select.Option>
-                    </Select>
-                    &nbsp;&nbsp;&nbsp;&nbsp;最小花费 :&nbsp;
-                    <Space><InputNumber defaultValue={0}/><Button type="primary">确定</Button></Space>
-                </div>
-                <div>
-                    <Popover placement="bottomRight" title="筛选器" content={<div style={{width: "500px"}}>
-                        <Row>
-                            <Col span={6}>
-                                <div style={{paddingLeft: "2px"}}>年龄:</div>
-                                <Checkbox.Group
-                                    value={age}
-                                    onChange={(value) => {
-                                        setAge(value);
-                                    }}
-                                    style={{width: '100%'}}
-                                    options={
-                                        [
-                                            {value: "age2", label: "18-24岁"},
-                                            {value: "age3", label: "25-34岁"},
-                                            {value: "age4", label: "35-44岁"},
-                                            {value: "age5", label: "45-54岁"},
-                                            {value: "age6", label: "55-64岁"},
-                                            {value: "age7", label: "65岁以上"}
-                                        ]
+                <Row>
+                    <Col xxl={12} span={24}>
+                        <div style={{minWidth:"525px",marginBottom:"10px"}}>
+                            评分排序 :&nbsp;
+                            <Select
+                                showSearch
+                                style={{width: 200}}
+                                placeholder="选择排序字段"
+                                optionFilterProp="children"
+                                filterOption={(input: string, option) =>
+                                    option?.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                }
+                            >
+                                <Select.Option value="approas">广告回报率</Select.Option>
+                                <Select.Option value="ctr">点击率</Select.Option>
+                                <Select.Option value="frequency">频率</Select.Option>
+                                <Select.Option value="impression">展示数</Select.Option>
+                                <Select.Option value="cpc">每次点击费用</Select.Option>
+                                <Select.Option value="cpm">费用/千次</Select.Option>
+                                <Select.Option value="installs">安装次数</Select.Option>
+                                <Select.Option value="installfee">每次安装费用</Select.Option>
+                            </Select>
+                            &nbsp;&nbsp;&nbsp;&nbsp;最小花费 :&nbsp;
+                            <Space><InputNumber defaultValue={0}/><Button type="primary">确定</Button></Space>
+                        </div>
+                    </Col>
+                    <Col xxl={12} span={24}>
+                        <div style={{minWidth:"830px",marginBottom:"10px"}}>
+                            <Popover placement="bottomRight" title="筛选器" content={<div style={{width: "500px"}}>
+                                <Row>
+                                    <Col span={6}>
+                                        <div style={{paddingLeft: "2px"}}>年龄:</div>
+                                        <Checkbox.Group
+                                            value={age}
+                                            onChange={(value) => {
+                                                setAge(value);
+                                            }}
+                                            style={{width: '100%'}}
+                                            options={
+                                                [
+                                                    {value: "age2", label: "18-24岁"},
+                                                    {value: "age3", label: "25-34岁"},
+                                                    {value: "age4", label: "35-44岁"},
+                                                    {value: "age5", label: "45-54岁"},
+                                                    {value: "age6", label: "55-64岁"},
+                                                    {value: "age7", label: "65岁以上"}
+                                                ]
+                                            }
+                                        >
+                                        </Checkbox.Group>
+                                    </Col>
+                                    <Col span={6}>
+                                        <div style={{paddingLeft: "2px"}}>姓别:</div>
+                                        <Checkbox.Group
+                                            value={sex}
+                                            style={{width: '100%'}}
+                                            onChange={(value) => {
+                                                setSex(value);
+                                            }}
+                                            options={
+                                                [
+                                                    {value: "male", label: "男"},
+                                                    {value: "female", label: "女"},
+                                                    {value: "unkown", label: "未分类"},
+                                                ]
+                                            }
+                                        >
+                                        </Checkbox.Group>
+                                    </Col>
+                                    <Col span={6}>
+                                        <div style={{paddingLeft: "2px"}}>设备:</div>
+                                        <Checkbox.Group
+                                            value={device}
+                                            onChange={(value) => {
+                                                setDevice(value);
+                                            }}
+                                            style={{width: '100%'}}
+                                            options={
+                                                [
+                                                    {value: "pc", label: "桌面"},
+                                                    {value: "mobile", label: "移动"}
+                                                ]
+                                            }
+                                        >
+                                        </Checkbox.Group>
+                                    </Col>
+                                    <Col span={6}>
+                                        <div style={{paddingLeft: "2px"}}>刊登位置:</div>
+                                        <Checkbox.Group
+                                            value={publishLocation}
+                                            onChange={(value) => {
+                                                setPublishLocation(value);
+                                            }}
+                                            style={{width: '100%'}}
+                                            options={
+                                                [
+                                                    {value: "facebook", label: "脸书"},
+                                                    {value: "instagram", label: "移动"},
+                                                    {value: "messenger", label: "信使"},
+                                                    {value: "audnetwork", label: "观众网络"}
+                                                ]
+                                            }
+                                        >
+                                        </Checkbox.Group>
+                                    </Col>
+                                </Row>
+                                <Row style={{paddingTop: "20px"}}>
+                                    <Col span={24}>
+                                        <Select mode="tags" style={{display: 'block'}} placeholder='选择国家' onChange={(value) => {
+                                            setCountry(value);
+                                        }} value={country} showSearch>
+                                            {
+                                                allCountry.map(country => {
+                                                    return <Select.Option key={country.code}
+                                                                          value={country.value}>{country.value}</Select.Option>
+                                                })
+                                            }
+                                        </Select>
+                                    </Col>
+                                </Row>
+                                <Row style={{paddingTop: "20px"}}>
+                                    <Col span={24}>
+                                        <div style={{display: "flex", justifyContent: "space-between"}}>
+                                            <Button onClick={() => {
+                                                setAge(undefined);
+                                                setSex(undefined);
+                                                setDevice(undefined);
+                                                setCountry(undefined);
+                                                setPublishLocation(undefined);
+                                            }}>清除筛选</Button>
+                                            <Button type="primary">套用筛选器</Button>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </div>} trigger="click">
+                                <Button type="link">筛选器</Button>
+                            </Popover>
+                            <span className={styles.date}>发布范围筛选： <DateRange onChange={(dateStr) => {
+                                setDate(dateStr);
+                                setPageIndex(1);
+                                dispatch({
+                                    type: 'advertising/fetchAdvList',
+                                    payload: {
+                                        current: 1,
+                                        start: dateStr[0],
+                                        end: dateStr[1],
+                                        size: pageSize
                                     }
-                                >
-                                </Checkbox.Group>
-                            </Col>
-                            <Col span={6}>
-                                <div style={{paddingLeft: "2px"}}>姓别:</div>
-                                <Checkbox.Group
-                                    value={sex}
-                                    style={{width: '100%'}}
-                                    onChange={(value) => {
-                                        setSex(value);
-                                    }}
-                                    options={
-                                        [
-                                            {value: "male", label: "男"},
-                                            {value: "female", label: "女"},
-                                            {value: "unkown", label: "未分类"},
-                                        ]
+                                });
+                            }}/></span>
+                            <span className={styles.date}>统计数据过滤： <DateRange onChange={(dateStr) => {
+                                setDateT(dateStr);
+                                setPageIndex(1);
+                                dispatch({
+                                    type: 'advertising/fetchAdvList',
+                                    payload: {
+                                        current: 1,
+                                        startT: dateStr[0],
+                                        endT: dateStr[1],
+                                        size: pageSize
                                     }
-                                >
-                                </Checkbox.Group>
-                            </Col>
-                            <Col span={6}>
-                                <div style={{paddingLeft: "2px"}}>设备:</div>
-                                <Checkbox.Group
-                                    value={device}
-                                    onChange={(value) => {
-                                        setDevice(value);
-                                    }}
-                                    style={{width: '100%'}}
-                                    options={
-                                        [
-                                            {value: "pc", label: "桌面"},
-                                            {value: "mobile", label: "移动"}
-                                        ]
-                                    }
-                                >
-                                </Checkbox.Group>
-                            </Col>
-                            <Col span={6}>
-                                <div style={{paddingLeft: "2px"}}>刊登位置:</div>
-                                <Checkbox.Group
-                                    value={publishLocation}
-                                    onChange={(value) => {
-                                        setPublishLocation(value);
-                                    }}
-                                    style={{width: '100%'}}
-                                    options={
-                                        [
-                                            {value: "facebook", label: "脸书"},
-                                            {value: "instagram", label: "移动"},
-                                            {value: "messenger", label: "信使"},
-                                            {value: "audnetwork", label: "观众网络"}
-                                        ]
-                                    }
-                                >
-                                </Checkbox.Group>
-                            </Col>
-                        </Row>
-                        <Row style={{paddingTop: "20px"}}>
-                            <Col span={24}>
-                                <Select mode="tags" style={{display: 'block'}} placeholder='选择国家' onChange={(value) => {
-                                    setCountry(value);
-                                }} value={country} showSearch>
-                                    {
-                                        allCountry.map(country => {
-                                            return <Select.Option key={country.code}
-                                                                  value={country.value}>{country.value}</Select.Option>
-                                        })
-                                    }
-                                </Select>
-                            </Col>
-                        </Row>
-                        <Row style={{paddingTop: "20px"}}>
-                            <Col span={24}>
-                                <div style={{display: "flex", justifyContent: "space-between"}}>
-                                    <Button onClick={() => {
-                                        setAge(undefined);
-                                        setSex(undefined);
-                                        setDevice(undefined);
-                                        setCountry(undefined);
-                                        setPublishLocation(undefined);
-                                    }}>清除筛选</Button>
-                                    <Button type="primary">套用筛选器</Button>
-                                </div>
-                            </Col>
-                        </Row>
-                    </div>} trigger="click">
-                        <Button type="link">筛选器</Button>
-                    </Popover>
-                    <span className={styles.date}>发布范围筛选： <DateRange onChange={(dateStr) => {
-                        setDate(dateStr);
-                        setPageIndex(1);
-                        dispatch({
-                            type: 'advertising/fetchAdvList',
-                            payload: {
-                                current: 1,
-                                start: dateStr[0],
-                                end: dateStr[1],
-                                size: pageSize
-                            }
-                        });
-                    }}/></span>
-                    <span className={styles.date}>统计数据过滤： <DateRange onChange={(dateStr) => {
-                        setDateT(dateStr);
-                        setPageIndex(1);
-                        dispatch({
-                            type: 'advertising/fetchAdvList',
-                            payload: {
-                                current: 1,
-                                startT: dateStr[0],
-                                endT: dateStr[1],
-                                size: pageSize
-                            }
-                        });
-                    }}/></span>
-                </div>
+                                });
+                            }}/></span>
+                        </div>
+                    </Col>
+                </Row>
             </div>
             <div>
                 <div className={styles.advWarp}>
