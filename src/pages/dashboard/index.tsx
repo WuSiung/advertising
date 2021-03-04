a675ba68031217d3e321834ca958d988c7ac0458
t-vic <t-vick@msn.com>
2021-03-04 18:26:51 +0800

    fix:修改仪表盘切换日期不更新数据的问题

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect, Dispatch } from 'umi';
 import { PageContainer } from '@ant-design/pro-layout';
 import { Row, Col, Card, Select, Space, Table, DatePicker } from 'antd';
@@ -43,6 +43,8 @@ const Dashboard: React.FC<DashboardProps> = (props) => {
   //   });
   // }, [dashboard.rangeValues]);
   const handleOpenChange = (open: boolean) => {
+    console.log(open);
+    console.log(dashboard.isRangeChanged);
     if (!open && dashboard.isRangeChanged) {
       dispatch({
         type: 'dashboard/queryStatistics',
@@ -255,6 +257,7 @@ const Dashboard: React.FC<DashboardProps> = (props) => {
       payload: {
         dates,
         dataStrings,
+        isRangeChanged: true
       },
     });
   };
