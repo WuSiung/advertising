0f5d19cce3b0f227195fc37887a90b941653a92f
wu <844155339@qq.com>
2021-03-10 09:21:39 +0800

    feat: filter data

--- src/pages/audience-manager/crowds/model.ts
+++ src/pages/audience-manager/crowds/model.ts
@@ -1,6 +1,7 @@
 import { Effect, Reducer } from 'umi'
 import { CrowdStateType } from './data';
 import { queryAudienceModelList } from '../manager/service';
+import { queryBaseCrowd } from './service';
 
 export interface ManagerModelType {
     namespace: string,
@@ -9,28 +10,61 @@ export interface ManagerModelType {
         fetchCrowdsList: Effect,
     },
     reducers: {
-        saveCrowdsList: Reducer<CrowdStateType['crowdsList']>,
+        saveCustomCrowd: Reducer<CrowdStateType['customCrowd']>,
+        saveBaseCrowd: Reducer<CrowdStateType['baseCrowd']>,
+        setTitle: Reducer<CrowdStateType>,
+        saveAllCrowd: Reducer<CrowdStateType['allCrowds']>,
+        setKinds: Reducer<CrowdStateType>,
     }
 }
 
 const CrowdsModel: ManagerModelType = {
     namespace: 'crowds',
     state: {
-        crowdsList: []
+        customCrowd: [],
+        baseCrowd: [],
+        title: '全部',
+        allCrowds: {
+            custom: [],
+            base: [],
+        },
+        kinds: 'all'
     },
     effects: {
         *fetchCrowdsList({ payload }, { call, put }) {
             const res = yield call(queryAudienceModelList, payload)
+            const baseRes = yield call(queryBaseCrowd, payload)
             yield put({
-                type: 'saveCrowdsList',
-                payload: { crowdsList: res.data.records }
+                type: 'saveCustomCrowd',
+                payload: { customCrowd: res.data.records }
+            })
+            yield put({
+                type: 'saveBaseCrowd',
+                payload: { baseCrowd: baseRes.data.records }
+            })
+            yield put({
+                type: 'saveAllCrowd',
+                payload: { allCrowds: { custom: res.data.records, base: baseRes.data.records } }
             })
         }
     },
     reducers: {
-        saveCrowdsList(state, { payload }) {
+        saveCustomCrowd(state, { payload }) {
             return { ...state, ...payload }
-        }
+        },
+        saveBaseCrowd(state, { payload }) {
+            return { ...state, ...payload }
+        },
+        setTitle(state, { payload }) {
+            return { ...state, ...payload }
+        },
+        saveAllCrowd(state, { payload }) {
+            return { ...state, ...payload }
+        },
+        setKinds(state, { payload }) {
+            return { ...state, ...payload }
+        },
+
     }
 }
 
