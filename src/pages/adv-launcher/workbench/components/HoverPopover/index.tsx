957c37742fbc9bafc9a6e2167506c0ddbb325e8b
wu <844155339@qq.com>
2021-03-10 09:21:29 +0800

    fix: 修改弹出数据

--- src/pages/adv-launcher/workbench/components/HoverPopover/index.tsx
+++ src/pages/adv-launcher/workbench/components/HoverPopover/index.tsx
@@ -1,47 +1,43 @@
 import { Popover } from 'antd'
 import React, { FC } from 'react'
+import { AdvDataType } from '../../data'
 
 import styles from './index.less'
 
 interface PopoverDataProps {
-    cost: string | number, // 平均点击消耗金额
-    octr: string | number, // 点击转换比
-    installs: number | string, // 安装次数
-    impressions: number, // 展示次数
-    spend: number, // 消费总金额
-    roas: string, // 广告支出回报率
-    income: string, // 广告收益
+    data?: AdvDataType
 }
 
 const PopoverContent: FC<PopoverDataProps> = (props) => {
+    const { data } = props
     return <div className={styles.datas}>
         <div className={styles.line}>
             <div className={styles.name}>广告收益</div>
-            <div className={styles.value}>${props.income || '0'}</div>
+            <div className={styles.value}>${data?.income || '0'}</div>
         </div>
         <div className={styles.line}>
             <div className={styles.name}>展示次数</div>
-            <div className={styles.value}>{props.impressions || '0'}</div>
+            <div className={styles.value}>{data?.impressions || '0'}</div>
         </div>
         <div className={styles.line}>
             <div className={styles.name}>广告支出回报率(ROAS)</div>
-            <div className={styles.value}>{Number(props.roas || '0').toFixed(2)}倍</div>
+            <div className={styles.value}>{Number(data?.roas || '0').toFixed(2)}倍</div>
         </div>
         <div className={styles.line}>
             <div className={styles.name}>平均点击消耗金额</div>
-            <div className={styles.value}>${Number(props.cost || '0').toFixed(2)}</div>
+            <div className={styles.value}>${Number(data?.cost || '0').toFixed(2)}</div>
         </div>
         <div className={styles.line}>
             <div className={styles.name}>消费总金额</div>
-            <div className={styles.value}>${Number(props.spend || '0').toFixed(2)}</div>
+            <div className={styles.value}>${Number(data?.spend || '0').toFixed(2)}</div>
         </div>
         <div className={styles.line}>
             <div className={styles.name}>点击转换比</div>
-            <div className={styles.value}>{Number(props.octr || '0').toFixed(2)}</div>
+            <div className={styles.value}>{Number(data?.octr || '0').toFixed(2)}</div>
         </div>
         <div className={styles.line}>
             <div className={styles.name}>安装次数</div>
-            <div className={styles.value}>{props.installs || '0'}</div>
+            <div className={styles.value}>{data?.installs || '0'}</div>
         </div>
     </div>
 }
