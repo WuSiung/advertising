545553d4a7d3c9bf3c0bd87b89ad011a9fe5164f
wu <844155339@qq.com>
2021-03-13 18:01:08 +0800

    fix: update version

--- src/pages/version/index.tsx
+++ src/pages/version/index.tsx
@@ -8,10 +8,12 @@ const Version = () => {
     const versionDetail: string[] = [
         '新增服务条款、隐私政策、cookie政策对应页面',
         '已有广告接口变更',
+        '广告发布成功跳转已有广告',
+        '修复若干bug'
     ]
     return <Card>
         <div className={styles.title}>最新版本{pjson.version}更新说明</div>
-        <div className={styles.time}>2021-03-06 18:30</div>
+        <div className={styles.time}>2021-03-13 18:30</div>
         <div className={styles.detail}>
             <ol>
                 {
