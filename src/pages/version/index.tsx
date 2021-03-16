4b76271eb2d500ce76ad17e5fdd458cbb7cd016c
wu <844155339@qq.com>
2021-03-16 18:24:46 +0800

    fix: edit version info

--- src/pages/version/index.tsx
+++ src/pages/version/index.tsx
@@ -6,6 +6,12 @@ import styles from './index.less'
 
 const Version = () => {
     const versionDetail: string[] = [
+        '人群包列表滚动调整及样式修改',
+        '已有广告接口再次变更',
+        '公共素材库样式修改',
+        '修复若干bug'
+    ]
+    const now: string[] = [
         '新增服务条款、隐私政策、cookie政策对应页面',
         '已有广告接口变更',
         '广告发布成功跳转已有广告',
@@ -13,11 +19,22 @@ const Version = () => {
     ]
     return <Card>
         <div className={styles.title}>最新版本{pjson.version}更新说明</div>
-        <div className={styles.time}>2021-03-13 18:30</div>
+        <div className={styles.time}>2021-03-16 18:24</div>
         <div className={styles.detail}>
             <ol>
                 {
                     versionDetail.map(item => {
+                        return <li key={item}>{item}</li>
+                    })
+                }
+            </ol>
+        </div>
+        <div className={styles.title}>1.1.34更新说明</div>
+        <div className={styles.time}>2021-03-13 18:30</div>
+        <div className={styles.detail}>
+            <ol>
+                {
+                    now.map(item => {
                         return <li key={item}>{item}</li>
                     })
                 }
