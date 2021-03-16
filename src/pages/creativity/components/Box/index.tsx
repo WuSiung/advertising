f72e90c1d27888a567cd988d00387be0b76440c5
wu <844155339@qq.com>
2021-03-16 18:13:53 +0800

    fix: 标签显示与box容器修改

--- src/pages/creativity/components/Box/index.tsx
+++ src/pages/creativity/components/Box/index.tsx
@@ -1,20 +1,21 @@
 import { isVideo, isImage } from '@/utils/fileType'
 import React, { FC } from 'react'
-import { Image } from 'antd'
+import { Image, Tag } from 'antd'
 import { CheckOutlined, CloseCircleOutlined, EditOutlined } from '@ant-design/icons'
 
 import styles from './index.less'
-import { PublicMaterialDataType } from '../../data'
+import { PublicMaterialDataType, TagType } from '../../data'
 
 type MaterialBox = {
     index: number,
     type?: 'media' | 'text',
     onClick?: (i: number) => void,
     onDelete?: (i: number) => void,
-    onEdit?: (i: number) => void
+    onEdit?: (i: number) => void,
+    tags: TagType[]
 } & PublicMaterialDataType
 const MaterialBox: FC<MaterialBox> = (props) => {
-    const { type, onClick, checked, index, onDelete, onEdit } = props
+    const { type, onClick, checked, index, onDelete, onEdit, tags } = props
     return <div className={`${styles.materialBox} ${type == 'media' ? styles.isMedia : styles.isText} ${checked ? styles.active : ''}`}
         onClick={() => onClick && onClick(index)}>
         {
@@ -28,10 +29,18 @@ const MaterialBox: FC<MaterialBox> = (props) => {
         {
             type == 'text' && <div className={styles.text}>{props.description + '&&' + props.title}</div>
         }
-        <span className={styles.rank}>{props.index + 1}</span>
         <span className={styles.edit} onClick={e => { e.stopPropagation(); onEdit && onEdit(index) }}><EditOutlined /></span>
         <span className={styles.close} onClick={e => { e.stopPropagation(); onDelete && onDelete(index) }}><CloseCircleOutlined /></span>
-        <span className={styles.check}><CheckOutlined /></span>
+        <div className={styles.tag}>
+            {
+                tags.map((tag, index) => {
+                    return index < 3 && <Tag color="magenta" key={tag.id}>{tag.name}</Tag>
+                })
+            }
+            {
+                tags.length > 3 && <Tag color="magenta">...</Tag>
+            }
+        </div>
     </div>
 }
 
