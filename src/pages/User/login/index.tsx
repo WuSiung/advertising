771a50e717f9d2462915469535a0ef487646f831
wu <844155339@qq.com>
2021-03-19 18:38:30 +0800

    style: login styles

--- src/pages/User/login/index.tsx
+++ src/pages/User/login/index.tsx
@@ -31,37 +31,30 @@ const Login: React.FC<LoginProps> = (props) => {
   };
   // loading结束后更新code 
   useEffect(() => { if (submitting == false) { setCodeRandom(randomLenNum(4, true)) } }, [submitting])
-  
+
   const changeRandom = useCallback((random: number) => { setCodeRandom(random) }, [codeRandom])
 
-  const layout = {
-    labelCol: { span: 6 },
-    wrapperCol: { span: 18 },
-  };
-  const tailLayout = {
-    wrapperCol: { offset: 6, span: 18 },
-  };
   return (
     <div className={styles.main}>
+      <div className={styles.title}>账号登陆</div>
       <Form
-        {...layout}
         name="basic"
         onFinish={(values) => {
           handleSubmit(values as LoginParamsType);
           return Promise.resolve();
         }}
       >
-        <Form.Item label="用户名" name="username" rules={[{ required: true, message: '请输入用户名' }]}>
-          <Input placeholder='请输入用户名' allowClear prefix={ <UserOutlined />}/>
+        <Form.Item name="username" rules={[{ required: true, message: '请输入用户名' }]}>
+          <Input placeholder='请输入用户名' allowClear prefix={<UserOutlined />} />
         </Form.Item>
-        <Form.Item label="密码" name="password" rules={[{ required: true, message: '请输入密码' }]}>
-          <Input.Password placeholder='请输入密码' prefix={ <LockOutlined /> }/>
+        <Form.Item name="password" rules={[{ required: true, message: '请输入密码' }]}>
+          <Input.Password placeholder='请输入密码' prefix={<LockOutlined />} />
         </Form.Item>
-        <Form.Item label="验证码">
+        <Form.Item>
           <Form.Item name="code" rules={[{ required: true, message: '请输入验证码' }]} noStyle><Input placeholder='请输入验证码' allowClear /></Form.Item>
           <RandomCode random={memoRandom} changeRandom={changeRandom} />
         </Form.Item>
-        <Form.Item {...tailLayout}>
+        <Form.Item>
           <Button type="primary" htmlType="submit">登陆</Button>
           <div className={styles.register} onClick={() => history.push('/user/register')}>没有账号？快来注册吧</div>
         </Form.Item>
