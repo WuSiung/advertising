6c6f2b73c1d45dac2489c3be94da4d03709cab3e
wu <844155339@qq.com>
2021-03-26 20:56:41 +0800

    fix: 系列命名规则修改

--- src/pages/adv-launcher/compaign/index.tsx
+++ src/pages/adv-launcher/compaign/index.tsx
@@ -233,7 +233,7 @@ const Compaign: FC<CompaignProps> = (props) => {
 }
 
 const initName = (appInfo: AppInfo): string => {
-    return 'TWzt-' + appInfo.appName + '-主要系列' + moment().format("YYYYMMDD")
+    return 'TWzt-' + appInfo.appName + '-' + moment().format("YYYYMMDD") + '-主要系列'
 }
 
 export default connect(({ workbench, compaigns, user, loading }: { workbench: WorkbenchDataType, compaigns: CompaignsData, user: UserModelState, loading: { effects: { [key: string]: boolean } } }) => ({
